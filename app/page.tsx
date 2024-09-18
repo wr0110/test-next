6c116681569a016340ad1c0d98faa53251986782
wr0110 <wittyraccoon0110@gmail.com>
2024-09-18 12:51:36 -0500

    added key prop

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import FAQ from "@/libs/components/faq";
 export default function Home() {
   return (
     <div className="flex flex-col gap-5 ml-5 mt-3">
-      {data.map((item) => (
-        <FAQ {...item} />
+      {data.map((item, index) => (
+        <FAQ key={index} {...item} />
       ))}
     </div>
   );
